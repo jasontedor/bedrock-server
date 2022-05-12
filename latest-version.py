b7808719f01f03bc11df232ff22d9f8ed5fb6b67
Jason Tedor <jason@tedor.me>
2022-05-12 14:48:05 -0400

    Update version finding logic

--- latest-version.py
+++ latest-version.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as bs
 
 def main():
   html = ulr.urlopen('https://minecraft.fandom.com/wiki/Bedrock_Dedicated_Server').read()
-  version = bs(html, 'html.parser').find_all(title=re.compile("Bedrock Dedicated Server"))[-1].text
+  version = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("th")[-1].text.strip()
   print(version)
 
 if __name__ == '__main__':
