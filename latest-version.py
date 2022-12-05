40eed8837c685dd0c5f58b7552b0257fd91190a8
Jason Tedor <jason@tedor.me>
2022-12-05 06:56:08 -0500

    Adjust version logic to exclude previews

--- latest-version.py
+++ latest-version.py
@@ -4,7 +4,12 @@ from bs4 import BeautifulSoup as bs
 
 def main():
   html = ulr.urlopen('https://minecraft.fandom.com/wiki/Bedrock_Dedicated_Server').read()
-  version = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("th")[-1].text.strip()
+  href = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("td")[-1].find("a")['href']
+  if "preview" not in href:
+    version_index = -1
+  else:
+    version_index = -2
+  version = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("th")[version_index].text.strip()
   print(version)
 
 if __name__ == '__main__':
