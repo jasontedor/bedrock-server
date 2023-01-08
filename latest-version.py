27a8a583bf420707a0b1448856f0f708dd267dc1
Jason Tedor <jason@tedor.me>
2023-01-08 12:21:53 -0500

    Account for Windows and previews

--- latest-version.py
+++ latest-version.py
@@ -4,12 +4,15 @@ from bs4 import BeautifulSoup as bs
 
 def main():
   html = ulr.urlopen('https://minecraft.fandom.com/wiki/Bedrock_Dedicated_Server').read()
-  href = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("td")[-1].find("a")['href']
-  if "preview" not in href:
-    version_index = -1
-  else:
-    version_index = -2
-  version = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("th")[version_index].text.strip()
+
+  index = -1
+  while True:
+    a_tag = bs(html, 'html.parser').find("span", {"id":"Download"}).parent.find_next("table").find_all("td")[index].find("a")
+    if "Linux" == a_tag.text and "preview" not in a_tag['href']:
+      break
+    index = index - 1
+
+  version = re.compile('.*bedrock-server-(.*).zip').match(a_tag['href']).group(1)
   print(version)
 
 if __name__ == '__main__':
